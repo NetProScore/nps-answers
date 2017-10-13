8c5d7438bffe07982b06e4e97a803cf750ac0c88
Tomasz Bawor <bawortomasz@gmail.com>
2017-10-13 15:31:43 +0200

    Initial commit

---
+++ src/main/java/com/netproscore/npsanswers/NpsAnswersApplication.java
@@ -0,0 +1,12 @@
+package com.netproscore.npsanswers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpsAnswersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpsAnswersApplication.class, args);
+	}
+}
